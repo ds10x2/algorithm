72fc5dea5e9c8dd6c31af71ad73cef51e947b6cf
ds10x2 <padasol@khu.ac.kr>
2023-07-08 15:28:49 +0900

    정렬

---
+++ 11399.cpp
@@ -0,0 +1,38 @@
+//4963
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> q;
+int sum[1001];
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int m;
+		cin >> m;
+		q.push(m);
+	}
+
+
+	for (int i = 1; i <= n; i++) {
+		sum[i] = sum[i - 1] + q.top();
+		cout << sum[i] << " ";
+		q.pop();
+	}
+
+
+	int result = 0;
+	for (int i = 1; i <= n; i++)
+		result += sum[i];
+
+	cout << result;
+
+	return 0;
+}
