64f92a276ae39a9b2cbb4a574edb32ca58cf3a56
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-09 22:35:40 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	long long n, m;
+	cin >> n >> m;
+	cout << abs(n - m);
+
+
+	return 0;
+}
