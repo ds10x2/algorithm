51c9abdbfeca910c1156b2a6e7063bfa9d2910b9
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-09 22:24:32 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	char c;
+	cin >> c;
+	cout << (int) c;
+
+	return 0;
+}
