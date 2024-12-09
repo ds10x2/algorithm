667b9ff76d0a9706bacc245b2491ff541a79bc7b
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-09 22:34:43 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	cout << "  ___  ___  ___" << "\n";
+	cout << "  | |__| |__| |" << "\n";
+	cout << "  |           |" << "\n";
+	cout << "   \\_________/" << "\n";
+	cout << "    \\_______/" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |_____|" << "\n";
+	cout << "  __/       \\__" << "\n";
+	cout << " /             \\" << "\n";
+	cout << "/_______________\\" << "\n";
+
+
+	return 0;
+}
