1e25717df5f44f37046a313891da52cd185daddb
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-09 22:21:19 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	string str;
+	cin >> str;
+
+	cout << str.length();
+
+	return 0;
+}
