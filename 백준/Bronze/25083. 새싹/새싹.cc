1bc893c3688bd0a8031f87934505b086986a1a6a
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-10 21:51:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	cout << "         ,r'\"7" << "\n";
+	cout << "r`-_   ,'  ,/" << "\n";
+	cout << " \\. \". L_r'" << "\n";
+	cout << "   `~\\/" << "\n";
+	cout << "      |" << "\n";
+	cout << "      |" << "\n";
+
+	return 0;
+}
