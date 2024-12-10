00540ce66308e1e7107e0e8a5d9cd4f150c5ab3d
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-10 23:11:55 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << "\n";
+
+
+	return 0;
+}
