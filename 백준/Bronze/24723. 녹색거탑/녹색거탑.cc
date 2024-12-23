63a137434f534b3c077f43fa320a53bc3cd93033
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-23 21:40:56 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	cout << pow(2, n) << "\n";
+
+	return 0;
+}
