45064b4d493e6d73abef575aa2a7bd0e56f1a9b2
박다솔 <108979449+ds10x2@users.noreply.github.com>
2024-12-23 21:39:58 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	cout << n * n - n << "\n";
+
+	return 0;
+}
